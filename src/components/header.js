4639f7293d418a418bc52a1f959099f8228cbe11
Sachira Chinthana Jayasanka <sachira@LIVE.COM>
2020-08-01 21:21:36 -0400

    Updated to contain blog and bio

--- src/components/header.js
+++ src/components/header.js
@@ -76,9 +76,9 @@ const Header = ({ siteTitle }) => (
   <SiteHeader>
     <Content>
       <p>
-        <HomeLink to="/">{siteTitle}</HomeLink>
-        <NavLink to="/blog">Blog</NavLink>
-        <GitHubLink href="https://github.com/niklasmtj/gatsby-starter-julia">
+        <HomeLink to="/">Blog</HomeLink>
+        <NavLink to="/bio">Bio</NavLink>
+        <GitHubLink href="https://github.com/SachiraChin">
           GitHub
         </GitHubLink>
       </p>

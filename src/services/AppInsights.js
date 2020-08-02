faf2384372e10c2674e9eba15973ff1a9f95883d
Sachira Chinthana Jayasanka <sachira@LIVE.COM>
2020-08-02 11:23:18 -0400

    Added new icon and application insights

---
+++ src/services/AppInsights.js
@@ -0,0 +1,20 @@
+// AppInsights.js
+import { ApplicationInsights } from '@microsoft/applicationinsights-web'
+import { ReactPlugin, withAITracking } from '@microsoft/applicationinsights-react-js'
+import { globalHistory } from "@reach/router"
+
+const reactPlugin = new ReactPlugin();
+const ai = new ApplicationInsights({
+    config: {
+        instrumentationKey: process.env.APPLICATION_INSIGHTS_KEY,
+        extensions: [reactPlugin],
+        extensionConfig: {
+            [reactPlugin.identifier]: { history: globalHistory }
+        }
+    }
+})
+if (process.env.APPLICATION_INSIGHTS_KEY)
+    ai.loadAppInsights()
+
+export default (Component) => withAITracking(reactPlugin, Component)
+export const appInsights = ai.appInsights
